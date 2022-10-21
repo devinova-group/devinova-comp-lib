71ef95c4be075cc9b49c754bbd266abf894f187d
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-10-21 10:34:10 +0200

    put the gitignore to loading branch

--- src/components/IconButton/IconButton.stories.tsx
+++ src/components/IconButton/IconButton.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import IconButton from "./IconButton";
-import { Plus } from "../../../IconAssets";
+import IconButton from "./IconButton.component";
+
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
