d3d90da8829677de3b04ac1069aa2027acbf8742
PettoDavida <maximilian.widman@gmail.com>
2022-10-25 11:29:11 +0200

    Changed import of spacing to units and created const for units.spacing

--- src/components/Toast/ToastContainer.ts
+++ src/components/Toast/ToastContainer.ts
@@ -1,6 +1,8 @@
-import styled, { CSSObject } from "@emotion/styled";
-import { spacing } from "../../units";
-import { portal } from "./ToastPortal";
+import styled, {CSSObject} from "@emotion/styled";
+import {units} from "../../units";
+import {portal} from "./ToastPortal";
+
+const spacing = units.spacing;
 
 const baseStyling: CSSObject = {
   width: "400px",
@@ -9,7 +11,7 @@ const baseStyling: CSSObject = {
   cursor: "pointer",
   display: "flex",
   flexDirection: "column",
-  gap: "1rem",
+  gap: spacing.d4,
   justifyContent: "center",
   alignItems: "center",
 };
@@ -17,23 +19,23 @@ const baseStyling: CSSObject = {
 type enumStyles = Record<string, CSSObject>;
 
 const positionVariants: enumStyles = {
-  "top-left": { top: spacing.d4, left: spacing.d4 },
+  "top-left": {top: spacing.d4, left: spacing.d4},
   "top-center": {
     top: spacing.d4,
     left: "50%",
     transform: "translateX(-50%)",
   },
-  "top-right": { top: spacing.d4, right: spacing.d4 },
-  "bottom-left": { bottom: spacing.d4, left: spacing.d4 },
+  "top-right": {top: spacing.d4, right: spacing.d4},
+  "bottom-left": {bottom: spacing.d4, left: spacing.d4},
   "bottom-center": {
     bottom: spacing.d4,
     left: "50%",
     transform: "translateX(-50%)",
   },
-  "bottom-right": { bottom: spacing.d4, right: spacing.d4 },
+  "bottom-right": {bottom: spacing.d4, right: spacing.d4},
 };
 
-const positionToastContainer = ({ position = "top-right" }: portal) =>
+const positionToastContainer = ({position = "top-right"}: portal) =>
   positionVariants[position];
 
 export const StyledToastContainer = styled.div<portal>(
