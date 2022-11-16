68dae2f36285862f99c3fb837a93038f6b8eebf9
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-11-16 09:49:13 +0100

    update style for button (#14)
    * update style for button
    
    * Refactored button to be more easily used as a component
    
    Co-authored-by: PettoDavida <maximilian.widman@gmail.com>

--- src/components/Button/Button.component.tsx
+++ src/components/Button/Button.component.tsx
@@ -5,6 +5,7 @@ import Icon from "../SvgIcons/Icon.component";
 import { css } from "@emotion/css";
 import { buttonHover } from "./Button.component.styles";
 
+
 const Button = ({
   variant,
   onClick,
@@ -17,73 +18,72 @@ const Button = ({
   isFullWidth,
   isHover,
   iconName,
-}: ButtonOption) => {
-  return (
-    <>
-      {typeof iconName == "string" && isHover && (
-        <ButtonComponent
-          variant={variant}
-          disabled={disabled}
-          color={color}
-          size={size}
-          style={style}
-          onClick={onClick}
-          isFullWidth={isFullWidth}
-          isHover={isHover}
-          iconName={iconName}
-          className={css({
-            [`.icon`]: {
-              fill: buttonHover[color].color,
-            },
-            [`&:hover .icon`]: {
-              display: "inline-flex",
-              fill: buttonHover[color].backgroundColor,
-            },
-          })}
+  }:  ButtonOption,
+  ) => {
+      return(
+        <>
+  {   typeof iconName =="string" && isHover && 
+      <ButtonComponent
+              variant={variant}
+              disabled={disabled}
+              color={color}
+              size={size}
+              style={style}
+              onClick={onClick}
+              isFullWidth={isFullWidth}
+              isHover={isHover}
+              iconName={iconName}
+              className={css({
+                [`.icon`]:{
+                  fill:color != undefined ? buttonHover[color].color : "white",
+                },
+                [`&:hover .icon`]: {
+                  display: "inline-flex",
+                  fill: color != undefined ? buttonHover[color].backgroundColor: "#4C3A80",
+                }
+              })}   >
+        <Icon name={iconName} className="icon" size={size}/>
+        {children}
+        </ButtonComponent> }
+  {   typeof iconName =="string" && !isHover && 
+      <ButtonComponent
+        variant = {variant}
+        color={color}
+        disabled={disabled}
+        size={size}
+        style={style}
+        onClick={onClick}
+        isFullWidth={isFullWidth}
+        isHover={isHover}
+        iconName={iconName}
+        className={css({
+          [`.icon`]:{
+            fill:color != undefined ? buttonHover[color].backgroundColor : "white",
+          }
+        })}
         >
-          <Icon name={iconName} className="icon" size={size} />
-          {children}
-        </ButtonComponent>
-      )}
-      {typeof iconName == "string" && !isHover && (
+        <Icon name={iconName} className="icon"  size={size}/>
+        {children}
+        </ButtonComponent> }   
+  
+  {     typeof iconName !=="string" && 
         <ButtonComponent
-          variant={variant}
-          color={color}
-          disabled={disabled}
-          size={size}
-          style={style}
-          onClick={onClick}
-          isFullWidth={isFullWidth}
-          isHover={isHover}
-          iconName={iconName}
-          className={css({
-            [`.icon`]: {
-              fill: buttonHover[color].backgroundColor,
-            },
-          })}
+        variant = {variant}
+        color={color}
+        disabled={disabled}
+        size={size}
+        style={style}
+        className={className}
+        onClick={onClick}
+        isFullWidth={isFullWidth}
+        isHover={isHover}
         >
-          <Icon name={iconName} className="icon" size={size} />
-          {children}
-        </ButtonComponent>
-      )}
-
-      {typeof iconName !== "string" && (
-        <ButtonComponent
-          variant={variant}
-          color={color}
-          disabled={disabled}
-          size={size}
-          style={style}
-          className={className}
-          onClick={onClick}
-          isFullWidth={isFullWidth}
-          isHover={isHover}
-        >
-          {children}
-        </ButtonComponent>
-      )}
-    </>
-  );
-};
-
-export default Button;
+        {children}
+        </ButtonComponent>}
+  
+        </>
+      )      
+      
+    };
+  
+export default Button
